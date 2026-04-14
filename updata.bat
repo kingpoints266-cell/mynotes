49ae0622f5f32f382fa782be4d1101a20682ea0a
Your Name <you@example.com>
2026-04-14 13:00:57 +0800

    auto update 周二 2026/04 13:00:57

--- updata.bat
+++ updata.bat
@@ -1,4 +1,7 @@
 @echo off
+:: 👇 就是这行代码！强制将命令行切换为 UTF-8 编码，>nul 是为了隐藏切换提示
+chcp 65001 >nul
+
 echo 🔄 正在保存本地笔记...
 git add .
 git commit -m "auto update %date:~0,10% %time:~0,8%"
@@ -9,5 +12,5 @@ git pull origin main --rebase
 echo ⬆️ 正在推送到 GitHub...
 git push origin main
 
-echo ✅ 更新与同步完成！Update Complete!
+echo ✅ 更新与同步完成！
 pause
